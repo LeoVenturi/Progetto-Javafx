34044946dde71cc3a309ba8f298a8adf3f547ea2
Victor Coiman <victor.coiman@studenti.univr.it>
2022-07-10 17:26:57 +0200

    V 0.1

--- Progetto JFX/src/main/java/com/example/provajfx/ControllerAccesso.java
+++ Progetto JFX/src/main/java/com/example/provajfx/ControllerAccesso.java
@@ -11,15 +11,18 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import com.example.provajfx.Medico;
 
 // sono stato qyua
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ControllerAccesso extends Medico{
+public class ControllerAccesso {
 
-    @FXML
+
+
+	@FXML
     private TextField UsenameTextfield;
     @FXML
     private TextField PasswordTextfield;
@@ -29,13 +32,33 @@ public class ControllerAccesso extends Medico{
     private Stage stage;
     private Scene scene;
     private Parent root;
+    
+    private Medico m1 = new Medico("1", "1");
+    private Medico m2 = new Medico("2", "2");
+    private Medico m3 = new Medico("3", "3");
+    private Medico m4 = new Medico("4", "4");
+    
+    private ArrayList<Medico> listaMedici = new ArrayList<>();
+    
+    private boolean trovaMedico(String nome, String pass) {
+    	for(Medico i: listaMedici) {
+    		if( i.findMed_Username(nome, pass))
+    			return true;
+    	}
+    	return false;
+    }
 
 
     public void accedi(ActionEvent event) throws IOException{
         String user = UsenameTextfield.getText();
         String pass = PasswordTextfield.getText();
+        listaMedici.add(m1);
+        listaMedici.add(m2);
+        listaMedici.add(m3);
+        listaMedici.add(m4);
+        
 
-		if (findMed_Username(pass, user)) {	// fallisce qua, mettendo false va meglio
+		if (trovaMedico(user, pass)) {	// fallisce qua, mettendo false va meglio
 			root = FXMLLoader.load(getClass().getResource("InterfacciaMedico.fxml"));
 			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
---
+++ Progetto JFX/src/main/java/com/example/provajfx/Farmacologo.java
@@ -0,0 +1,5 @@
+package com.example.provajfx;
+
+public class Farmacologo {
+
+}
