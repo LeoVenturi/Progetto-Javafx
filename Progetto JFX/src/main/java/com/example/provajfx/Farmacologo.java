47328ed2d5f3ef23fca5ea5c5cb9f3a795b06d5d
pozzi <pozzi@DESKTOP-GIDKM6E.home>
2022-07-10 19:21:57 +0200

    versione 0.02

--- Progetto JFX/src/main/java/com/example/provajfx/Farmacologo.java
+++ Progetto JFX/src/main/java/com/example/provajfx/Farmacologo.java
@@ -1,9 +1,35 @@
 package com.example.provajfx;
 
-public class Farmacologo {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Farmacologo extends Accessi{
+	private ArrayList<Medico> medici = new ArrayList<>();
+	private boolean segnale = false;
+	
+	public Farmacologo(String pass, String nomeUtente, ArrayList<Medico> medici) {
+		super(pass, nomeUtente);
+		this.medici = medici;
+		
+	}
+	
+	public void mandaSegnale() {
+		this.segnale = true;
+	}
+	
+	public void deleteSegnale() {
+		this.segnale = false;
+	}
+	public boolean find_utente(String user, String password) {
+		return super.findMed_Username(user, password);
+		
+	}
 	
 	
+	public String toString() {
+		return null;
+	}
 	
 	
 
